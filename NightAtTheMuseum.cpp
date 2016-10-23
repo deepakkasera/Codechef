fd9ba7f9888daa9906eaf732b404dea62e8ca1a8
deepakkasera <deepak.kasera143.dk@gmail.com>
2016-10-23 13:49:07 +0530

    codechef

---
+++ NightAtTheMuseum.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	ios_base::sync_with_stdio(false);
+	string s;
+	cin>>s;
+	char last = 'a';
+	int len = s.length();
+	int count=0;
+	for(auto i : s)
+		cout<<i<<" ";
+	return 0;
+}
